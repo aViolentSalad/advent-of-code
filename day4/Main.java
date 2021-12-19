7cd0e8d62fd961e81a238320cdf21ef3725af240
aViolentsalad <wenjunjie14@gmail.com>
2021-12-19 23:57:12 +0800

    day4 added

---
+++ day4/Main.java
@@ -0,0 +1,16 @@
+package day4;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String fileName = "day3/input.txt";
+        FileInputStream file = new FileInputStream(fileName);
+        Kattio io = new Kattio(file, System.out);
+        ArrayList<Integer> boards = new ArrayList<>();
+        ArrayList<Integer>  = new ArrayList<>();
+        
+        io.close();
+    }
+}
